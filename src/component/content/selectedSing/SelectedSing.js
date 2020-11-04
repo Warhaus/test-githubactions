228ee56f84f0a646d639e2ba5469da0f8bc33b7f
ArthurDorokhin <a.u.dorokhin@gmail.com>
2020-11-04 17:28:45 +0200

    horoscope

--- src/component/content/selectedSing/SelectedSing.js
+++ src/component/content/selectedSing/SelectedSing.js
@@ -5,15 +5,28 @@ function MainPage() {
     const horoscopeSingSelected = useSelector(state => state.horoscopeSing.horoscopeSingSelected)
     const horoscopeSingData = useSelector(state => state.horoscopeSing.horoscopeSingData)
 
+
+
+
+    const date = new Date();
+    const day = date.getDay()
+    const monthsArr = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const months = monthsArr[date.getMonth()].substr(0, 3)
+    const year = date.getFullYear();
+
     return (
-        <div className="container">
+        <div className="wrap-bg">
+            <div className="bg"><img src="/img/daily-banner-bg.png" alt=""/></div>
             {horoscopeSingData.filter(item => item.id === horoscopeSingSelected).map(item =>
-                <div className="group-zodiac-selected" key={item.id}>
-                    <div className="name"><p>{item.name}</p></div>
-                    <div className="description">{item.description}</div>
+                <div className="container" key={item.id}>
+                    <div className="group-zodiac-selected">
+                        <div className="name"><p>{item.name} Horoscope</p></div>
+                        <div className="description"><p><strong>{months} {day}, {year}</strong> - {item.description}</p></div>
+                    </div>
                 </div>
             )}
         </div>
+
     );
 }
 
